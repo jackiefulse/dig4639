467fd2dc6f0b94ea772a53c368b071607c24a0ea
Jacqueline Fulse <fulsej64@knights.ucf.edu>
2019-02-11 14:07:06 -0500

    create lab2

---
+++ lab2/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff9999',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
